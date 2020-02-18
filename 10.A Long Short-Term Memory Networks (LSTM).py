979212adeb9c701361e3658be875a0b1ffb2503c
Hao <hao.quan@icloud.com>
2020-02-18 18:05:40 +0100

    Delete unuseful datasets

--- 10.A Long Short-Term Memory Networks (LSTM).py
+++ 10.A Long Short-Term Memory Networks (LSTM).py
@@ -4,12 +4,12 @@ import torchvision.transforms as transforms
 import torchvision.datasets as dsets
 from torch.autograd import Variable
 
-train_dataset = dsets.MNIST(root='./',
+train_dataset = dsets.MNIST(root='./data',
                             train=True,
                             transform=transforms.ToTensor(),
                             download=True)
 
-test_dataset = dsets.MNIST(root='./root',
+test_dataset = dsets.MNIST(root='./data',
                            train=False,
                            transform=transforms.ToTensor(),
                            download=True)
