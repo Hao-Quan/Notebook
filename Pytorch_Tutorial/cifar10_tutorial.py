b496e8ed26c08d4ed0beb5ed9ec057c12ac20ac0
Hao <hao.quan@icloud.com>
2020-02-23 23:49:38 +0100

    23-02-2020

--- Pytorch_Tutorial/cifar10_tutorial.py
+++ Pytorch_Tutorial/cifar10_tutorial.py
@@ -3,54 +3,13 @@ import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import numpy as np
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
 
 # TODO
 # transforms = transforms.Compose([transforms.ToTensor(),
 #      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-# TODO MODIFY
-trainset = torchvision.datasets.CIFAR10(root='./data',
-                                        train=True,
-                                        download=True,
-                                        transform=transforms.ToTensor())
-
-testset = torchvision.datasets.CIFAR10(root='./data',
-                                        train=False,
-                                        download=True,
-                                        transform=transforms.ToTensor())
-
-trainloader = torch.utils.data.DataLoader(trainset,
-                                          batch_size=4,
-                                          shuffle=True,
-                                          num_workers=2)
-
-testloader = torch.utils.data.DataLoader(testset,
-                                         batch_size=4,
-                                         shuffle=False,
-                                         num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-
-def imshow(img):
-
-    img = img / 2 + 0.5
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
-
-
-# get some random training images
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
-
-# show images
-imshow(torchvision.utils.make_grid(images))
-print(''.join('%5s' % classes[labels[j]] for j in range(4)))
-
-import torch.nn as nn
-import torch.nn.functional as F
 
 class Net(nn.Module):
     def __init__(self):
@@ -71,38 +30,138 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
 
+# TODO MODIFY
+trainset = torchvision.datasets.CIFAR10(root='./data',
+                                        train=True,
+                                        download=True,
+                                        transform=transforms.ToTensor())
+
+testset = torchvision.datasets.CIFAR10(root='./data',
+                                        train=False,
+                                        download=True,
+                                        transform=transforms.ToTensor())
+
+trainloader = torch.utils.data.DataLoader(trainset,
+                                          batch_size=4,
+                                          shuffle=True,
+                                          num_workers=0)
+
+testloader = torch.utils.data.DataLoader(testset,
+                                         batch_size=4,
+                                         shuffle=False,
+                                         num_workers=0)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+def imshow(img):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+print(''.join(' %5s' % classes[labels[j]] for j in range(4)))
+
+
 net = Net()
 
-import torch.optim as optim
-
+print("")
 learning_rate = 0.01
+
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=learning_rate)
+optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
 
-# TODO (trainloader, 0)
 
-for epoch in range(2):
-    running_loss = 0.0
+# for epoch in range(2):
+#     running_loss = 0.0
+#
+#     # enumerate(trainloader, 0) == enumerate(trainloader, 0)
+#     # it determine from which order number starts, as default, it is 0
+#
+#     for i, (inputs, labels) in enumerate(trainloader, 0):
+#         optimizer.zero_grad()
+#
+#         outputs = net(inputs)
+#         loss = criterion(outputs, labels)
+#         loss.backward()
+#         optimizer.step()
+#
+#         running_loss += loss.item()
+#
+#         if i % 2000 == 1999:
+#             print('[%d, %5d] loss: %.3f' %
+#                   (epoch + 1, i + 1, running_loss / 2000))
+#             running_loss = 0.0
+#
+# print('Finished Training')
+#
+PATH = './cifar_net.pth'
+# torch.save(net.state_dict(), PATH)
 
-    # enumerate(trainloader, 0) == enumerate(trainloader, 0)
-    # it determine from which order number starts, as default, it is 0
+dataiter = iter(testloader)
+images, labels = dataiter.next()
 
-    for i, (inputs, labels) in enumerate(trainloader, 0):
-        optimizer.zero_grad()
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+net = Net()
+net.load_state_dict(torch.load(PATH))
+net.eval()
 
-        running_loss += loss.item()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
 
-        if i % 2000 == 1999:
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
+outputs = net(images).to(device)
 
-print('Finished Training')
+_, predicted = torch.max(outputs, 1)
+
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(device), data[1].to(device)
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' %
+          (classes[i], 100 * class_correct[i] / class_total[i]))
+
+print("")
+
+
+
+
+
 
 
 
